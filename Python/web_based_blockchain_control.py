719ae4641f2a67a48c5c6fb2667147f3735b0f46
V1leDev <vle.official.ap@gmail.com>
2020-04-18 16:15:18 +0200

    Add basic api structure

--- Python/web_based_blockchain_control.py
+++ Python/web_based_blockchain_control.py
@@ -1,5 +1,5 @@
 from Python import blockchain as bc
-from flask import Flask
+from flask import Flask, jsonify
 from uuid import uuid4
 
 knotenpunkt = Flask(__name__)
@@ -7,3 +7,19 @@ einzigartiger_name_knotenpunkt = str(uuid4()).replace('-', '')
 blockchain = bc.Blockchain
 
 
+@knotenpunkt.route('/mine')
+def minen():
+    pass
+
+
+@knotenpunkt.route('/chain')
+def rückgabe_ganze_blockchain():
+    antwort = {
+        'chain': blockchain.chain,
+        'länge': len(blockchain.chain)
+    }
+    return jsonify(antwort), 200
+
+@knotenpunkt.route('/transaktionen/neu')
+def neue_transaktion():
+    pass
