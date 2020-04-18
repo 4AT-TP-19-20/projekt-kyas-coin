3246c0f525cfa415b2a8a9553854a3246b49f41f
V1leDev <vle.official.ap@gmail.com>
2020-04-18 13:50:04 +0200

    Add basic blockchain class

---
+++ Python/blockchain.py
@@ -0,0 +1,16 @@
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.aktuelle_transaktion = []
+
+    def neuer_block(self):
+        pass
+
+    def neue_transaktion(self):
+        pass
+
+    def block_hashen(self):
+        pass
+
+    def letzter_block(self):
+        pass
