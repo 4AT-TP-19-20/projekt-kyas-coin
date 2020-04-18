9403e3461d13670335bfbc6e581c2085c4377eef
V1leDev <vle.official.ap@gmail.com>
2020-04-18 18:10:10 +0200

    Add code for creating new transaction through api

--- Python/web_based_blockchain_control.py
+++ Python/web_based_blockchain_control.py
@@ -1,5 +1,5 @@
 from Python import blockchain as bc
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from uuid import uuid4
 
 knotenpunkt = Flask(__name__)
@@ -15,7 +15,7 @@ def minen():
     # Belohnung für Mining einbauen
 
     vorheriger_hash = blockchain.block_hashen(vorheriger_block)
-    neuer_block = blockchain.neuer_block(beweis, vorheriger_hash)
+    neuer_block = blockchain.neuer_block(beweis=beweis, vorheriger_hash=vorheriger_hash)
     antwort = {
         'nachricht': "Neuer Block wurde erstellt",
         'index': neuer_block['index'],
@@ -37,4 +37,11 @@ def rückgabe_ganze_blockchain():
 
 @knotenpunkt.route('/transaktionen/neu')
 def neue_transaktion():
-    pass
+    transaktion_inputs = request.get_json()
+
+    index_transaktion = blockchain.neue_transaktion(absender=transaktion_inputs['absender'],
+                                                    empfänger=transaktion_inputs['empfänger'],
+                                                    betrag=transaktion_inputs['betrag'])
+    antwort = {'nachricht': f'Transaktion wird zum Block hinzugefügt mit dem index {index_transaktion}'}
+    return jsonify(antwort), 201
+
