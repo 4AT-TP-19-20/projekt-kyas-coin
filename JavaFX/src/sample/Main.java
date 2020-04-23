13e2f864d393b2c7f0cda484d7fdafd4d8f27379
V1leDev <vle.official.ap@gmail.com>
2020-04-23 16:58:24 +0200

    Refactor gui look

--- JavaFX/src/sample/Main.java
+++ JavaFX/src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         mainstage = primaryStage;
         mainstage.setTitle("");
         mainstage.setScene(new Scene(root));
+        mainstage.setResizable(false);
         mainstage.show();
     }
 
