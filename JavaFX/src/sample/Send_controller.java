0029ff56882c8dfea8b025494044e937ac251955
V1leDev <vle.official.ap@gmail.com>
2020-04-23 13:24:53 +0200

    Add minor alert message refactoring

--- JavaFX/src/sample/Send_controller.java
+++ JavaFX/src/sample/Send_controller.java
@@ -36,7 +36,7 @@ public class Send_controller extends Login_controller{
 
     public void synchronize(ActionEvent actionEvent) throws IOException {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setContentText("Press okay to synchronize blockchain\nPlease wait until finished");
+        alert.setContentText("Press okay to synchronize balance\nPlease wait until finished");
         alert.showAndWait();
         Settings_controller.api.get_balance();
     }
